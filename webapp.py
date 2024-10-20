54737a44bf71fa782be181044730d44b1f751f5a
ozbeko@sheridancollege.ca <ozbeko@sheridancollege.ca>
2024-10-20 15:19:27 -0400

    Routes to Home, Monitor, and About pages have been defined and tested.

--- webapp.py
+++ webapp.py
@@ -3,5 +3,13 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
-def initial_test():
-    return "<p>Hello World! This is the initial test</p>"
+def render_home():
+    return "<h1 style='color:red; text-align:center;'>Render Home Page here</h1>"
+
+@app.route("/monitor")
+def render_monitor():
+    return "<h1 style='color:red; text-align:center;' >Render Monitor Page here</h1>"
+
+@app.route("/about")
+def render_about():
+    return "<h1 style='color:red; text-align:center;'>Render About Page here</h1>"
