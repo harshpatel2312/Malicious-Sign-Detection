2080687a73822f4163ef30bfbf1beb367fa6916a
ozbeko@sheridancollege.ca <ozbeko@sheridancollege.ca>
2025-03-01 17:25:16 -0500

    The logic for getting metrics has been seperated as an individual function.

--- webapp.py
+++ webapp.py
@@ -4,23 +4,26 @@ import requests
 
 app=Flask(__name__)
 
+def get_metrics():
+    try:
+        response = requests.get('http://127.0.0.1:5000/data')
+        return response.json() if response.status_code == 200 else {}
+    except requests.exceptions.RequestException as e:
+        print(f"Fetching metrics is failed: {e}")
+        return {}
+
 @app.route("/")
 def render_home():
     return render_template("home.html")
 
-@app.route("/monitor")
-def render_monitor():
-    try:
-        response = requests.get('http://127.0.0.1:5000/data')
-        metrics = response.json() if response.status_code == 200 else {}
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching colors: {e}")
-        metrics ={}
-    return render_template("monitor.html", metrics=metrics)
-
 @app.route("/about")
 def render_about():
     return render_template("about.html")
+
+@app.route("/monitor")
+def render_monitor():
+    metrics = get_metrics()
+    return render_template("monitor.html", metrics=metrics)
 
 if __name__=="__main__":
     app.run(host='127.0.0.1', port=5001, debug=True)
