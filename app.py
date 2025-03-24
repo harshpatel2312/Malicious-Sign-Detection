225d46da36e8190ca8bd9d0081474c2b486ddb05
ozbeko@sheridancollege.ca <ozbeko@sheridancollege.ca>
2025-03-24 14:03:18 -0400

    Application has been containerized. Host number has been changed to make it accessible remotely. Port forwarding has been set up, too. Then, it has been tested successfully for accessing remotely while running on container.

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def render_monitor():
                            chart4_html=chart4_html)
 
 if __name__=="__main__":
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
